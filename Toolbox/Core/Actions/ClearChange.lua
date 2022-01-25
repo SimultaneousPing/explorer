c2dd4750de10ee342f5224c3a51f69b8c4befe1d
SimultaneousPing <91077273+SimultaneousPing@users.noreply.github.com>
2022-01-25 08:27:56 -0500

    Create ClearChange.lua

---
+++ Toolbox/Core/Actions/ClearChange.lua
@@ -0,0 +1,16 @@
+local Plugin = script.Parent.Parent.Parent
+local FFlagToolboxDeduplicatePackages = game:GetFastFlag("ToolboxDeduplicatePackages")
+local Libs
+if FFlagToolboxDeduplicatePackages then
+	Libs = Plugin.Packages
+else
+	Libs = Plugin.Libs
+end
+local Util = require(Libs.Framework).Util
+local Action = Util.Action
+
+return Action(script.Name, function(setting)
+	return {
+		setting = setting
+	}
+end)
