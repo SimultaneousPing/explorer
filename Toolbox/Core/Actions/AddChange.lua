1f760f3ea5b44d94952e35407f3862988a351403
SimultaneousPing <91077273+SimultaneousPing@users.noreply.github.com>
2022-01-25 08:24:40 -0500

    Create AddChange.lua

---
+++ Toolbox/Core/Actions/AddChange.lua
@@ -0,0 +1,17 @@
+local Plugin = script.Parent.Parent.Parent
+local FFlagToolboxDeduplicatePackages = game:GetFastFlag("ToolboxDeduplicatePackages")
+local Libs
+if FFlagToolboxDeduplicatePackages then
+	Libs = Plugin.Packages
+else
+	Libs = Plugin.Libs
+end
+local Util = require(Libs.Framework).Util
+local Action = Util.Action
+
+return Action(script.Name, function(setting, value)
+	return {
+		setting = setting,
+		value = value,
+	}
+end)
