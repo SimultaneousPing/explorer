42180164422308500578ddedfb8dc1066e4ac8be
SimultaneousPing <91077273+SimultaneousPing@users.noreply.github.com>
2022-01-25 08:25:28 -0500

    Create ChangeBackground.lua

---
+++ Toolbox/Core/Actions/ChangeBackground.lua
@@ -0,0 +1,19 @@
+local Plugin = script.Parent.Parent.Parent
+
+local FFlagToolboxDeduplicatePackages = game:GetFastFlag("ToolboxDeduplicatePackages")
+local Libs
+if FFlagToolboxDeduplicatePackages then
+	Libs = Plugin.Packages
+else
+	Libs = Plugin.Libs
+end
+local Util = require(Libs.Framework).Util
+local Action = Util.Action
+
+return Action(script.Name, function(settings, index, selected)
+	return {
+		settings = settings,
+		index = index,
+		selected = selected,
+	}
+end)
